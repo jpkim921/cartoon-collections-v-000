793a07c83ddbedd68fdbfe23c4e7e52525190d8e
jpkim921 <jpkim921@gmail.com>
2018-01-18 02:32:51 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,10 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)# code an argument here
+  array.each_with_index { |name, idx| puts "#{idx + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.map{|element| element.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
